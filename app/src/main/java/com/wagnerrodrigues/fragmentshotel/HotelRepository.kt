9892a26fd5e2fa0ac61209fdb86bb6331c2a0027
wagner <wagnersr92@gmail.com>
2020-11-08 21:50:14 -0300

    Commit inicial projeto Hotel

---
+++ app/src/main/java/com/wagnerrodrigues/fragmentshotel/HotelRepository.kt
@@ -0,0 +1,8 @@
+package com.wagnerrodrigues.fragmentshotel
+
+interface HotelRepository{
+    fun save(hotel: Hotel)
+    fun remove(vararg hotels: Hotel)
+    fun hotelById(id: Long, callback: (Hotel?) -> Unit)
+    fun search(term: String, callback: (List<Hotel>)->Unit)
+}
